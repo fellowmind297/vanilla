3022d5835b75539ca220cdf499ef8e9e5ee69d85
Jesper Bøgholm <jesperb@proactive.dk>
2021-03-16 15:33:30 +0100

    update

--- api/GetMessage/run.ps1
+++ api/GetMessage/run.ps1
@@ -12,10 +12,10 @@ if (-not $name) {
     $name = $Request.Body.Name
 }
 
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+$body = "{ text: ""This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."" }"
 
 if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
+    $body = "{ text: ""Hello, $name. This HTTP triggered function executed successfully."" }"
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
